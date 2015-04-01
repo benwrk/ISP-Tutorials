4f9a0bd67421cce41e1bbf8cebb48def26e1f613
Benjapol Worakan <benwrk@live.com>
2015-04-01 13:53:05 +0700

    TUT 103 - Initial commit

---
+++ TUT103 - PacMan/src/GameLayer.js
@@ -0,0 +1,51 @@
+var GameLayer = cc.LayerColor.extend({
+	init: function() {
+		this._super(new cc.Color(127, 127, 127, 255));
+		this.setPosition(0, 0);
+		console.log('Initialized');
+		
+		this.scoreLabel = cc.LabelTTF.create('0', 'Arial', 40);
+		this.scoreLabel.setPosition(screenWidth - 50, screenHeight - 50);
+		this.addChild(this.scoreLabel);
+		
+		this.addKeyboardHandlers();
+		this.scheduleUpdate();
+		
+		return true;
+	},
+	
+	onKeyDown: function(keyCode, event) {
+		console.log('KeyDown: ' + keyCode.toString());
+	},
+	
+	onKeyUp: function(keyCode, event) {
+		// console.log('KeyUp: ' + keyCode.toString());
+	},
+	
+	addKeyboardHandlers: function() {
+		var self = this;
+		cc.eventManager.addListener({
+			event: cc.EventListener.KEYBOARD,
+			onKeyPressed: function(keyCode, event) {
+				self.onKeyDown(keyCode, event);
+			},
+			onKeyReleased: function(keyCode, event) {
+				self.onKeyUp(keyCode, event);
+			}
+		}, this);
+	},
+	
+	update: function() {
+		
+	}
+});
+
+var StartScene = cc.Scene.extend({
+	onEnter: function() {
+		this._super();
+		var layer = new GameLayer();
+		console.log('GameLayer created');
+		layer.init();
+		this.addChild(layer);
+	}
+});
---
+++ TUT103 - PacMan/src/resource.js
@@ -0,0 +1,9 @@
+var res = {
+    //HelloWorld_png : "res/HelloWorld.png",
+	//Ship_png : "res/Ship.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
