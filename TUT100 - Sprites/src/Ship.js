73b3ee1f093b1ed3d0ec0493bb4a86350244d367
Benjapol Worakan <benwrk@live.com>
2015-04-01 10:32:59 +0700

    TUT 100 - update: function(dt) now handles directions.

--- TUT100 - Sprites/src/Ship.js
+++ TUT100 - Sprites/src/Ship.js
@@ -1,16 +1,43 @@
-
 var Ship = cc.Sprite.extend({
 	ctor: function() {
 		this._super();
 		this.initWithFile('res/images/Ship.png');
 		this.setScale(0.0625, 0.0625);
+		
+		this.direction = Ship.DIR.UP;		
 	},
 
 	update: function(dt) {
-		this.setPosition(this.getPositionX(), this.getPositionY() + 5);
+		if (direction === Ship.DIR.UP) {
+			this.setPosition(this.getPositionX(), this.getPositionY() + 5);
+		} else if (direction === Ship.DIR.RIGHT) {
+			this.setPosition(this.getPositionX() + 5, this.getPositionY());
+		} else if (direction === Ship.DIR.DOWN) {
+			this.setPosition(this.getPositionX(), this.getPositionY() - 5);
+		} else if (direction === Ship.DIR.LEFT) {
+			this.setPosition(this.getPositionX() - 5, this.getPositionY());
+		}
+		
 		if (this.getPositionY() > screenHeight) {
 			this.setPositionY(0);
 		}
-		//console.log('Ship.update | ' + this._name + ' | setPosition(' + this.getPositionX() + ', ' + this.getPositionY() + ')');
+		if (this.getPositionX() > screenWidth) {
+			this.setPositionX(0);
+		}
+		if (this.getPositionY() < 0) {
+			this.setPositionY(screenHeight);
+		}
+		if (this.getPositionX() < 0) {
+			this.setPositionX(screenWidth);
+		}
+		// console.log('Ship.update | ' + this._name + ' | setPosition(' +
+		// this.getPositionX() + ', ' + this.getPositionY() + ')');
 	}
 });
+
+Ship.DIR = {
+		UP: 1,
+		RIGHT: 2,
+		DOWN: 3,
+		LEFT: 4
+};
